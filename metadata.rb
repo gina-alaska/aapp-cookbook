fa5e7489a8d115489f63d75b5e7a9f66c9a3b5b2
Scott Macfarlane <scott@gina.alaska.edu>
2015-09-17 09:56:04 -0800

    Version bump

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'support+chef@gina.alaska.edu'
 license 'APACHE2'
 description 'Installs/Configures aapp'
 long_description 'Installs/Configures aapp'
-version '0.1.0'
+version '0.2.0'
 
 depends 'yum-gina'
 depends 'line'
